09a03b382e2bd0e02acd812e0228411f2da7fd1a
ameldekok <ameldekok@live.nl>
2025-09-30 12:32:57 +0000

    Refactor taxi trips and zones assets to include partitioning; add adhoc request asset and related jobs

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/schedules.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+import dagster as dg
+from dagster_essentials.defs.jobs import trip_update_job, weekly_update_job
+
+trip_update_schedule = dg.ScheduleDefinition(
+    job=trip_update_job,
+    cron_schedule="0 0 5 * *" # Every 5th of month at midnight
+    )
+
+weekly_update_schedule = dg.ScheduleDefinition(
+    job=weekly_update_job,
+    cron_schedule="0 0 * * 1"
+)
---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/sensors.py
@@ -0,0 +1,6 @@
+import dagster as dg
+import os
+import json
+
+from dagster_essentials.defs.jobs import adhoc_request_job
+
